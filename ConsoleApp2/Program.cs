7755d39ee46e01d98f32cad1962101c9d613199e
AlexKovani <95691143+AlexKovani@users.noreply.github.com>
2022-01-29 20:21:13 +0300

    Update Program.cs

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -18,14 +18,14 @@ namespace ConsoleApp2
                 int d = Convert.ToInt32(Console.ReadLine());
                 Console.WriteLine("Введите длину прогрессии: ");
                 int num = Convert.ToInt32(Console.ReadLine());
-                ArithProgression A_progress = new ArithProgression(firstnum, d, num);
-                //progress.setStart(firstnum);
+                ArithProgression A_progress = new ArithProgression(d, num);
+                A_progress.setStart(firstnum);
                 A_progress.getNext();
                 Console.WriteLine();
                 A_progress.reset();
                 Console.WriteLine();
-                Geomrogression G_progress = new Geomrogression(firstnum, d, num);
-                //progress.setStart(firstnum);
+                Geomrogression G_progress = new Geomrogression(d, num);
+                G_progress.setStart(firstnum);
                 G_progress.getNext();
                 Console.WriteLine();
                 G_progress.reset();
